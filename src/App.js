e83864397367624ad37cdd08f12a81b8e28e72e6
Your Name <you@example.com>
2022-08-14 12:39:37 +0530

    okie

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from "react"
 class App extends React.Component{
-render(){
-  return(
-    <div>
-      <h1>Jyothish</h1>
-    </div>
-  )
-
-}
+  render(){
+    return(<div>
+      <h1>React js</h1>
+      <hr></hr>
+      <h2>Html
+      </h2>
+    </div>)
+  }
 }
 export default App
