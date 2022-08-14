dc57ef5e8c34760005f3f240d98b7671eb6a4e4b
Your Name <you@example.com>
2022-08-15 00:50:56 +0530

    okie

--- src/components/class components/Four.jsx
+++ src/components/class components/Four.jsx
@@ -2,11 +2,13 @@ import React from "react"
 class Four extends React.Component{
     name="jyothish"
     location="nellore"
+    Mother_name="anu"
     father_details={name:"srinivasulu reddy",Age:45,occupation:"farmer"}
     render(){
         return(<>
         <h1>NAME:{this.name}</h1>
         <h1>LOCATION:{this.location}</h1>
+        <h1>Mother_name:{this.Mother_name}</h1>
         <h1>FATHER NAME :{this.father_details.name}</h1>
         <h1>FATHER AGE :{this.father_details.Age}</h1>
         </>
